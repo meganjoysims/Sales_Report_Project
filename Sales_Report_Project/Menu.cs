b601f3223c0f7c643f8edfcfa49dace472eedefc
Megan Sims <msims2808@googlemail.com>
2021-07-20 12:17:26 +0100

    further work completed

---
+++ Sales_Report_Project/Menu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales_Report_Project
+{
+    class Menu
+    {
+
+        public void MainMenu()
+        {
+            Console.WriteLine("Sales Report");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("");
+            Console.WriteLine("1.Data Entry");
+            Console.WriteLine("2.Reports");
+            Console.WriteLine("3.Exit");
+            int ch = Int32.Parse(Console.ReadLine());
+
+            if(ch == 1)
+            {
+                DataEntryMenu();
+            }
+
+            if(ch == 2)
+            {
+                ReportsMenu();
+            }
+
+            if (ch == 3)
+            {
+                Console.WriteLine("Thank you, goodbye");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+        }
+
+        public void DataEntryMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Information required to add data to sales report:");
+            Console.WriteLine("Sale ID, Product Name, Quantity Sold, Price");
+            Console.WriteLine("");
+            
+            //Console.WriteLine("Sale ID:");
+            //string saleID = Console.ReadLine();
+
+            Console.WriteLine("Product Name:");
+            string prodName = Console.ReadLine();
+
+            Console.WriteLine("Quantity sold:");
+            string quantity = Console.ReadLine();
+
+            Console.WriteLine("Price:");
+            string price = Console.ReadLine();
+
+            Console.Read();
+            functions sales = new functions();
+            sales.addData(prodName, quantity, price);
+        }
+
+        public void ReportsMenu()
+        {
+
+            Console.WriteLine("");
+            Console.WriteLine("Select option for which report to view:");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("1:Products sold by year");
+            Console.WriteLine("2:Products sold by month in year");
+            Console.WriteLine("3:Total sales by year");
+            Console.WriteLine("4:Total sales by month in year");
+            int ch2 = Int32.Parse(Console.ReadLine());
+        }
+
+    }
+}
---
+++ Sales_Report_Project/functions.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Sales_Report_Project
+{
+    class functions
+    {
+        MySqlConnection con;
+        MySqlCommand cmd;
+
+        public functions()
+        {
+            //connects to Nationwide database in MySql
+            string connectionstring = "server=localhost;user=root;password=root;database=sales";
+            con = new MySqlConnection(connectionstring);
+            con.Open();
+            cmd = new MySqlCommand();
+            cmd.Connection = con;
+        }
+
+        public void addData(string prodName, string quantity, string price)
+        {
+            
+            cmd.CommandText = $"select max(substr(SaleID,1,3)+1) from sales_report";
+            MySqlDataReader data = cmd.ExecuteReader();
+            data.Read();
+            string newSaleID = "00" + data[0];
+
+
+            string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+            data.Close();
+            cmd.CommandText = $"insert into sales_report values('{newSaleID}', '{prodName}', '{quantity}', '{price}', '{currentDate}')";
+            cmd.ExecuteNonQuery();
+
+            Console.ReadLine();
+            data.Close();
+        }
+
+    }
+}
